7d05cb70b06b2b4630dcc796902aee82d74cfada
taheeraahmed <taheera@hotmail.com>
2023-09-14 12:19:23 +0200

    Still 0.690?

--- IA/logistic_regression/logistic_regression.py
+++ IA/logistic_regression/logistic_regression.py
@@ -3,41 +3,51 @@ import pandas as pd
 
 class LogisticRegression:
 
-    def __init__(self, learning_rate=0.01, num_iterations=1000):
+    def __init__(self, learning_rate=0.01, num_iterations=1000, regularization=0.01, batch_size=32):
         """
-        Initialize the Logistic Regression model with hyperparameters.
+        Initialize the Logistic Regression model with batch gradient descent.
 
         Args:
             learning_rate (float): The learning rate for gradient descent.
             num_iterations (int): The number of training iterations.
+            regularization (float): Strength of L2 regularization.
+            batch_size (int): The number of samples in each mini-batch.
         """
         self.learning_rate = learning_rate
         self.num_iterations = num_iterations
+        self.regularization = regularization
+        self.batch_size = batch_size
         self.weights = None
         self.bias = None
 
     def fit(self, X, y):
         """
-        Estimates parameters for the classifier using gradient descent.
+        Estimates parameters for the classifier using batch gradient descent.
 
         Args:
             X (array<m,n>): a matrix of floats with m rows (#samples) and n columns (#features)
             y (array<m>): a vector of floats containing m binary 0.0/1.0 labels
         """
-        self.weights = np.zeros(X.shape[1])
+        m, n = X.shape
+        self.weights = np.zeros(n)
         self.bias = 0
 
         for _ in range(self.num_iterations):
-            # Compute predictions
-            y_pred = sigmoid(np.dot(X, self.weights) + self.bias)
+            for i in range(0, m, self.batch_size):
+                # Create mini-batch
+                X_batch = X[i:i + self.batch_size]
+                y_batch = y[i:i + self.batch_size]
 
-            # Compute gradients
-            dw = (1 / X.shape[0]) * np.dot(X.T, (y_pred - y))
-            db = (1 / X.shape[0]) * np.sum(y_pred - y)
+                # Compute predictions for the mini-batch
+                y_pred = sigmoid(np.dot(X_batch, self.weights) + self.bias)
 
-            # Update parameters
-            self.weights -= self.learning_rate * dw
-            self.bias -= self.learning_rate * db
+                # Compute gradients for the mini-batch
+                dw = (1 / X_batch.shape[0]) * np.dot(X_batch.T, (y_pred - y_batch)) + (2 * self.regularization * self.weights)
+                db = (1 / X_batch.shape[0]) * np.sum(y_pred - y_batch)
+
+                # Update parameters
+                self.weights -= self.learning_rate * dw
+                self.bias -= self.learning_rate * db
 
     def predict(self, X):
         """
@@ -53,7 +63,6 @@ class LogisticRegression:
         """
         if self.weights is None or self.bias is None:
             raise ValueError("Model not trained. Call fit() first.")
-
         y_pred = sigmoid(np.dot(X, self.weights) + self.bias)
         return y_pred
 
