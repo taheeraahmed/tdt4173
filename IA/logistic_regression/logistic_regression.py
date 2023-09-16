dc6aa793f1618c670fb6024f2625ef247fc37711
taheeraahmed <taheera@hotmail.com>
2023-09-16 17:22:52 +0200

    Best for dataset 1 0.930

--- IA/logistic_regression/logistic_regression.py
+++ IA/logistic_regression/logistic_regression.py
@@ -3,67 +3,52 @@ import pandas as pd
 
 
 class LogisticRegression:
-    def __init__(self, degree=1, learning_rate=0.01, num_iterations=1000, regularization=0.01, batch_size=32):
-        # Initialize parameters and hyperparameters
+    def __init__(self, degree=1, learning_rate=0.01, num_iterations=1000, regularization=0.01, batch_size=32, convergence_threshold=1e-4):
         self.degree = degree
         self.learning_rate = learning_rate
         self.num_iterations = num_iterations
         self.regularization = regularization
         self.batch_size = batch_size
+        self.convergence_threshold = convergence_threshold
+        
         self.bias = None
         self.weights = None
 
     def generate_polynomial_features(self, X):
-        # Generate polynomial features of specified degree
         X_poly = X.copy()
         for d in range(2, self.degree + 1):
             X_poly = np.concatenate((X_poly, X ** d), axis=1)
         return X_poly
 
     def fit(self, X, y):
-        # Initialize weights and bias
         X_poly = self.generate_polynomial_features(X)
         m, n = X_poly.shape
-        self.weights = np.zeros(n)
+        self.weights = np.random.randn(n)
         self.bias = 0
 
         for _ in range(self.num_iterations):
-            # Compute predictions for the entire dataset
-            y_pred = sigmoid(np.dot(X_poly, self.weights) + self.bias)
+            for i in range(0, m, self.batch_size):
+                # Create mini-batch
+                X_batch = X_poly[i:i + self.batch_size]
+                y_batch = y[i:i + self.batch_size]
 
-            # Compute gradients and update parameters
-            dw = (1 / m) * np.dot(X_poly.T, (y_pred - y)) + (2 * self.regularization * self.weights)
-            db = (1 / m) * np.sum(y_pred - y)
-            self.weights -= self.learning_rate * dw
-            self.bias -= self.learning_rate * db
+                # Compute predictions for the mini-batch
+                y_pred = sigmoid(np.dot(X_batch, self.weights) + self.bias)
+
+                # Compute gradients for the mini-batch
+                dw = (1 / X_batch.shape[0]) * np.dot(X_batch.T, (y_pred - y_batch)) + (2 * self.regularization * self.weights)
+                db = (1 / X_batch.shape[0]) * np.sum(y_pred - y_batch)
+
+                # Update parameters
+                self.weights -= self.learning_rate * dw
+                self.bias -= self.learning_rate * db
 
     def predict(self, X):
-        # Generate predictions
         if self.weights is None or self.bias is None:
             raise ValueError("Model not trained. Call fit() first.")
         X_poly = self.generate_polynomial_features(X)
         y_pred = sigmoid(np.dot(X_poly, self.weights) + self.bias)
         return y_pred
-
-    def evaluate(self, X, y, threshold=0.5):
-        # Evaluate model performance
-        y_pred = self.predict(X)
-        accuracy = self.binary_accuracy(y, y_pred, threshold)
-        loss = self.binary_cross_entropy(y, y_pred)
-        return accuracy, loss
-
-    @staticmethod
-    def binary_accuracy(y_true, y_pred, threshold=0.5):
-        # Binary classification accuracy
-        y_pred_thresholded = (y_pred >= threshold).astype(float)
-        correct_predictions = y_pred_thresholded == y_true
-        return correct_predictions.mean()
-
-    @staticmethod
-    def binary_cross_entropy(y_true, y_pred, eps=1e-15):
-        # Binary cross-entropy loss
-        y_pred = np.clip(y_pred, eps, 1 - eps)
-        return -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
 
 
 # --- Some utility functions
